440a6fee3a18333dc0b4b69066e0bff29a02986b
Zuzanna <242509@edu.p.lodz.pl>
2023-04-05 13:18:44 +0200

    dodano podstawowe elementy klasy Orb

---
+++ Billiard/Data/Orb.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Data
+{
+    public class Orb
+    {
+        private int radius;
+        private int x;
+        private int y;
+
+        public Orb(int radius, int x, int y)
+        {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+        }
+
+        void changePosition() //domyslnym kwalifikatorem dostepu jest internal
+        {
+            Random rnd = new Random();
+            x += rnd.Next(-10,10); //na razie przykładowe liczby (-10,10)
+            y += rnd.Next(-10,10);
+        }
+
+    }
+}
