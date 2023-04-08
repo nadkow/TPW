373b89168290ca8c6f072ff43dd0dc1498506cc2
Zuzanna <242509@edu.p.lodz.pl>
2023-04-09 00:01:22 +0200

    Dodano funkcjonalnosc przycisku Start z wykorzystaniem interfejsu ICommand, ilosc kul jest przekazywana warstwie Data

---
+++ Billiard/ViewModel/StartButtonCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class StartButtonCommand : ICommand
+    {
+        ViewModelApi buttonViewModel;
+        public StartButtonCommand(ViewModelApi viewModel)
+        {
+            buttonViewModel = viewModel;
+        }
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true; // czyli przycisk jest klikalny
+        }
+
+        public void Execute(object? parameter)
+        {
+            buttonViewModel.ButtonStartClick();
+        }
+    }
+}
--- Billiard/ViewModel/ViewModelApi.cs
+++ Billiard/ViewModel/ViewModelApi.cs
@@ -1,23 +1,34 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Model;
 
 namespace ViewModel
 {
     public class ViewModelApi
     {
+        private ModelAbstractApi modelApi;
+        public StartButtonCommand ButtonCommand {get; set;}
         private int _ilosc;
         public int Ilosc
         {
             get { return _ilosc; }
             set { 
                 _ilosc = Convert.ToInt32(value);
-                OnPropertyChanged();
             }
         }
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        public ViewModelApi()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            ButtonCommand = new StartButtonCommand(this);
+            modelApi = new ModelApi();
+        }
+        public void Start()
+        {
+            this.modelApi.Start(_ilosc);
+        }
+
+        public void ButtonStartClick()
+        {
+            Start();
         }
     }
 }
