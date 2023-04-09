82184a29cc07c386d109ff7ea2e4d5aa5c5ed3c2
242435 Kowalska Nadia <242435@edu.p.lodz.pl>
2023-04-09 03:32:43 +0200

    Dodanie klasy Circle - wizualnej reprezentacji Orb

---
+++ Billiard/Model/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class Circle
+    {
+        // wizualna reprezentacja Orb
+        private int x;
+        private int y;
+        private int diameter;
+    }
+}
--- Billiard/ViewModel/ViewModelApi.cs
+++ Billiard/ViewModel/ViewModelApi.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Model;
 
@@ -7,6 +8,7 @@ namespace ViewModel
     public class ViewModelApi
     {
         private ModelAbstractApi modelApi;
+        private ObservableCollection<Circle> balls;
         public StartButtonCommand ButtonCommand {get; set;}
         private int _ilosc;
         public int Ilosc
@@ -16,6 +18,9 @@ namespace ViewModel
                 _ilosc = Convert.ToInt32(value);
             }
         }
+
+        public ObservableCollection<Circle> Balls { get => balls; set => balls = value; }
+
         public ViewModelApi()
         {
             ButtonCommand = new StartButtonCommand(this);
