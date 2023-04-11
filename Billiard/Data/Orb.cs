ba0352d3863311aca1580ce629cff0083e3b7812
242435 Kowalska Nadia <242435@edu.p.lodz.pl>
2023-04-11 22:17:01 +0200

    Dodano interfejs IOrb

--- Billiard/Data/Orb.cs
+++ Billiard/Data/Orb.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace Data
 {
-    public class Orb : INotifyPropertyChanged
+    public class Orb : IOrb, INotifyPropertyChanged
     {
         private double x;
         private double y;
